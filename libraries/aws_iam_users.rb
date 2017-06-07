a352d23846bd63d5f4003b98fb9c66464aadcbc4
Simon Varlow <simon.varlow@desire2learn.com>
2017-06-07 14:24:04 -0400

    Adding Filter table and Collect User Details to aws_iam_users.rb
    Signed-off-by: Chris Redekop <chris.redekop@d2l.com>

--- libraries/aws_iam_users.rb
+++ libraries/aws_iam_users.rb
@@ -8,7 +8,7 @@ class AwsIamUsers < Inspec.resource(1)
     filter = FilterTable.create
     filter.add_accessor(:where)
           .add_accessor(:entries)
-          .add(:user_name, field: :user)
+          .add(:user_name, field: :username)
           .add(:has_mfa_enabled?)
           .add(:has_console_password?)
     filter.connect(self, :collect_user_details)
@@ -20,9 +20,15 @@ class AwsIamUsers < Inspec.resource(1)
   end
 
   def collect_user_details
+<<<<<<< 862ab67b3707ee4fa61ef05fccf8ccd1abdd5b1a
     @users_cache ||= @user_provider.list_users unless @user_provider.nil?
   end
 
+=======
+    @users_cache ||= @user_provider.collect_user_details unless @user_provider.nil?
+  end
+  
+>>>>>>> Adding Filter table and Collect User Details to aws_iam_users.rb
   def users
     users = []
     users ||= @user_provider.list_users unless @user_provider.nil?
