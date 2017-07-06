strataconsulting/inspec-aws

Adding Filter table and Collect User Details to aws_iam_users.rb
Signed-off-by: Simon Varlow <simon.varlow@desire2learn.com>

libraries/aws_iam_users.rb
@@ -5,12 +5,24 @@ class AwsIamUsers < Inspec.resource(1)
   desc 'Verifies settings for AWS IAM users'
   example ''
 
+    filter = FilterTable.create
+    filter.add_accessor(:where)
+          .add_accessor(:entries)
+          .add(:user_name, field: :username)
+          .add(:has_mfa_enabled?)
+          .add(:has_console_password?)
+    filter.connect(self, :collect_user_details)
+
   def initialize(aws_user_provider = AwsIam::UserProvider.new,
                  user_factory = AwsIamUserFactory.new)
     @user_provider = aws_user_provider
     @user_factory = user_factory
   end
 
+  def collect_user_details
+    @users_cache ||= @user_provider.collect_user_details unless @user_provider.nil?
+  end
+  
   def users
     users ||= @user_provider.list_users unless @user_provider.nil?
 

chef-boneyard/inspec-aws-old

Signed-off-by: Chris Redekop <chris.redekop@d2l.com>

libraries/aws_iam_users.rb
@@ -5,13 +5,26 @@ class AwsIamUsers < Inspec.resource(1)
   desc 'Verifies settings for AWS IAM users'
   example ''
 
+    filter = FilterTable.create
+    filter.add_accessor(:where)
+          .add_accessor(:entries)
+          .add(:user_name, field: :user)
+          .add(:has_mfa_enabled?)
+          .add(:has_console_password?)
+    filter.connect(self, :collect_user_details)
+
   def initialize(aws_user_provider = AwsIam::UserProvider.new,
                  user_factory = AwsIamUserFactory.new)
     @user_provider = aws_user_provider
     @user_factory = user_factory
   end
 
+  def collect_user_details
+    @users_cache ||= @user_provider.list_users unless @user_provider.nil?
+  end
+
   def users
+    users = []
     users ||= @user_provider.list_users unless @user_provider.nil?
 
     users.map { |user|