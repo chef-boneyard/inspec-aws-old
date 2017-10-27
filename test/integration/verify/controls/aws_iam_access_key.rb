d402756ec7e9f296ff803bb519213db7ad5ab6ee
Clinton Wolfe <clintoncwolfe@gmail.com>
2017-10-27 14:26:43 -0400

    Failing integration tests for aws_iam_access_keys
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- test/integration/verify/controls/aws_iam_access_key.rb
+++ test/integration/verify/controls/aws_iam_access_key.rb
@@ -1,3 +1,28 @@
+access_key_user = attribute(
+  'access_key_user',
+  default: 'default.access_key_user',
+  description: 'Name of IAM user access_key_user')
+
+access_key_id = attribute(
+    'access_key_arn',
+    default: 'default.access_key_user',
+    description: 'Access Key ID of access key of IAM user access_key_user')
+
 describe aws_iam_access_key(username: 'not-a-user', 'id': 'not-an-id') do
   it { should_not exist }
+end
+
+describe aws_iam_access_key(username: access_key_user, 'id': access_key_id) do
+  it { should exist }
+  # TODO - check last used, created, other key metadata
+end
+
+describe aws_iam_access_keys do
+  it { should exist }
+end
+
+describe aws_iam_access_keys.where(username: access_key_user) do
+  its('length') { should be 1 }
+  it { should include access_key_id }
+  its('first.id') { should be access_key_id } 
 end
