2c72b1fcb60e515622adb5132e3ef2d2d89e5c31
Clinton Wolfe <clintoncwolfe@gmail.com>
2017-10-30 10:57:34 -0400

    Failing unit test for filter methods
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- test/unit/resources/aws_iam_access_keys_test.rb
+++ test/unit/resources/aws_iam_access_keys_test.rb
@@ -3,8 +3,15 @@ require 'aws-sdk'
 require 'helper'
 require 'aws_iam_access_keys'
 
-class AwsIamAccessKeysTest < Minitest::Test
+class AwsIamAccessKeysConstructorTest < Minitest::Test
   def test_bare_constructor_does_not_explode
     AwsIamAccessKeys.new
+  end
+end
+
+class AwsIamAccessKeysFilterTest < Minitest::Test
+  def test_filter_method_where_should_exist
+    resource = AwsIamAccessKeys.new
+    assert_respond_to(resource, :where)
   end
 end
