6655a1961d2157c70f0cafe2cd15e6032b41bc1d
Clinton Wolfe <clintoncwolfe@gmail.com>
2017-10-30 11:58:03 -0400

    Failing exists tests
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- libraries/aws_iam_access_keys.rb
+++ libraries/aws_iam_access_keys.rb
@@ -19,4 +19,36 @@ class AwsIamAccessKeys < Inspec.resource(1)
   def load_access_key_data
     []
   end
+
+  # Internal support class.  This is used to fetch
+  # the users and access keys.  We have an abstract
+  # class with a concrete AWS implementation provided here;
+  # a mock implementation is also provided in the unit tests.
+  class AccessKeyProvider
+    # Implementation of AccessKeyProvider which operates by looping over
+    # all users, then fetching their access keys.
+    # TODO: An alternate, more scalable implementation could be made
+    # using the Credential Report.
+    class AwsUserIterator < AccessKeyProvider
+      def fetch(filter_criteria)
+        raise 'unimplemented concrete method'
+      end
+    end
+
+    DEFAULT_PROVIDER = AwsIamAccessKeys::AccessKeyProvider::AwsUserIterator
+    @selected_implementation = DEFAULT_PROVIDER
+
+    # Use this to change what class is created by create().
+    def self.select(klass)
+      @selected_implementation = klass
+    end
+
+    def self.create
+      @selected_implementation.new
+    end
+
+    def fetch(_filter_criteria)
+      raise 'Unimplemented abstract method'
+    end
+  end
 end
--- test/unit/resources/aws_iam_access_keys_test.rb
+++ test/unit/resources/aws_iam_access_keys_test.rb
@@ -3,15 +3,76 @@ require 'aws-sdk'
 require 'helper'
 require 'aws_iam_access_keys'
 
+#==========================================================#
+#                 Constructor Tests                        #
+#==========================================================#
+
 class AwsIamAccessKeysConstructorTest < Minitest::Test
   def test_bare_constructor_does_not_explode
     AwsIamAccessKeys.new
   end
 end
 
+#==========================================================#
+#                   Filtering Tests                        #
+#==========================================================#
+
 class AwsIamAccessKeysFilterTest < Minitest::Test
-  def test_filter_method_where_should_exist
+  # Reset provider back to the implementation default prior
+  # to each test.  Tests must explicitly select an alternate.
+  def setup
+    AwsIamAccessKeys::AccessKeyProvider.select(AwsIamAccessKeys::AccessKeyProvider::DEFAULT_PROVIDER)
+  end
+
+  def test_filter_methods_should_exist
     resource = AwsIamAccessKeys.new
-    assert_respond_to(resource, :where)
+    [:where, :exists].each do |meth|
+      assert_respond_to(resource, meth)
+    end
+  end
+
+  def test_filter_method_where_should_be_chainable
+    AwsIamAccessKeys::AccessKeyProvider.select(AlwaysEmptyMAKP)
+    resource = AwsIamAccessKeys.new
+    assert_kind_of(AwsIamAccessKeys, resource.where)
+  end
+
+  def test_filter_method_exists_should_probe_empty_when_empty
+    AwsIamAccessKeys::AccessKeyProvider.select(AlwaysEmptyMAKP)
+    resource = AwsIamAccessKeys.new
+    refute(resource.exists)
+  end
+
+  def test_filter_method_exists_should_probe_present_when_present
+    AwsIamAccessKeys::AccessKeyProvider.select(BasicMAKP)
+    resource = AwsIamAccessKeys.new
+    assert(resource.exists)
+  end
+end
+
+#==========================================================#
+#                 Mock Support Classes                     #
+#==========================================================#
+
+# MAKP = MockAccessKeyProvider.  Abbreviation not used
+# outside this file.
+
+class AlwaysEmptyMAKP < AwsIamAccessKeys::AccessKeyProvider
+  def fetch
+    []
+  end
+end
+
+class BasicMAKP < AwsIamAccessKeys::AccessKeyProvider
+  def fetch
+    [
+      {
+        username: 'bob',
+        access_key_id: 'AKIA1234567890ABCDEF',
+        created_date: '2017-10-27T17:58:00Z',
+        # last_used_date: Time.now,  Seperate API call aws iam get-access-key-last-used --access-key-id ...
+        status: 'Active',
+      }
+    ]
   end
 end
