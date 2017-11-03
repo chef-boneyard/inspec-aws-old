76c3e768b10b96b56642f32987c026cdf9824698
Clinton Wolfe <clintoncwolfe@gmail.com>
2017-11-03 14:26:19 -0400

    Unit tests for search
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- test/unit/resources/aws_cloudwatch_log_metric_filter_test.rb
+++ test/unit/resources/aws_cloudwatch_log_metric_filter_test.rb
@@ -1,9 +1,13 @@
+require 'ostruct'
 require 'helper'
 require 'aws_cloudwatch_log_metric_filter'
 
 # CWLMF = CloudwatchLogMetricFilter
 # Abbreviation not used outside this file
 
+#=============================================================================#
+#                            Constructor Tests
+#=============================================================================#
 class AwsCWLMFConstructor < Minitest::Test
   def setup
     AwsCloudwatchLogMetricFilter::Backend.select(AwsMockCWLMFBackend::Empty)
@@ -28,7 +32,107 @@ class AwsCWLMFConstructor < Minitest::Test
   end
 end
 
-class AwsMockCWLMFBackend < AwsCloudwatchLogMetricFilter::Backend
-  class Empty
+#=============================================================================#
+#                             Search Tests                                    #
+#=============================================================================#
+class AwsCWLMFSearch < Minitest::Test
+  def setup
+    # Reset to the Basic kit each time 
+    AwsCloudwatchLogMetricFilter::Backend.select(AwsMockCWLMFBackend::Basic)
+  end
+
+  def test_using_lg_and_lmf_name_when_exactly_one
+    lmf = AwsCloudwatchLogMetricFilter.new(
+      log_group_name: 'test-log-group-01',
+      filter_name: 'test-01',
+    )
+    assert lmf.exists?
+  end
+
+  def test_using_lg_and_lmf_name_when_not_present
+    lmf = AwsCloudwatchLogMetricFilter.new(
+      log_group_name: 'test-log-group-01',
+      filter_name: 'test-1000-nope',
+    )
+    refute lmf.exists?
+  end
+
+  def test_using_log_group_name_resulting_in_duplicates
+    assert_raises(RuntimeError) do 
+      AwsCloudwatchLogMetricFilter.new(
+        log_group_name: 'test-log-group-01'
+      )
+    end
+  end
+
+  def test_duplicate_locally_uniqued_using_pattern
+    lmf = AwsCloudwatchLogMetricFilter.new(
+      log_group_name: 'test-log-group-01',
+      pattern: 'INFO',
+    )
+    assert lmf.exists?
+  end
+
+end
+#=============================================================================#
+#                            Property Tests                                   #
+#=============================================================================#
+
+#=============================================================================#
+#                 Support Classes - Mock Data Providers                       #
+#=============================================================================#
+class AwsMockCWLMFBackend
+  class Empty < AwsCloudwatchLogMetricFilter::Backend
+    def describe_metric_filters(_criteria)
+      []
+    end
+  end
+  class Basic < AwsCloudwatchLogMetricFilter::Backend
+    def describe_metric_filters(criteria)
+      everything = [
+        OpenStruct::new({
+          filter_name: 'test-01',
+          filter_pattern: 'ERROR',
+          log_group_name: 'test-log-group-01',
+          metric_transformations: [
+            OpenStruct.new({
+              metric_name: 'alpha',
+              metric_namespace: 'awesome_metrics',
+            })
+          ]
+        }),
+        OpenStruct::new({
+          filter_name: 'test-01', # Intentional duplicate 
+          filter_pattern: 'ERROR',
+          log_group_name: 'test-log-group-02',
+          metric_transformations: [
+            OpenStruct.new({
+              metric_name: 'beta',
+              metric_namespace: 'awesome_metrics',
+            })
+          ]
+        }),
+        OpenStruct::new({
+          filter_name: 'test-03',
+          filter_pattern: 'INFO',
+          log_group_name: 'test-log-group-01',
+          metric_transformations: [
+            OpenStruct.new({
+              metric_name: 'gamma',
+              metric_namespace: 'awesome_metrics',
+            })
+          ]
+        }),
+      ]
+      #  byebug  
+      selection = everything
+      # Here we filter on anything the AWS SDK lets us filter on remotely
+      # - which notably does not include the 'pattern' criteria
+      [:log_group_name, :filter_name].each do |remote_filter|
+        next unless criteria.key?(remote_filter)
+        selection.select! {|lmf| lmf[remote_filter] == criteria[remote_filter]}
+      end
+      selection
+    end
   end
 end
