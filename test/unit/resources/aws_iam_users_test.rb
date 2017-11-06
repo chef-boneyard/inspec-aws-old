da40ced0b97d9edac5e7fa2fa56d356c50a3b981
Clinton Wolfe <clintoncwolfe@gmail.com>
2017-11-06 17:05:11 -0500

    Basic 'where' test in place, no criteria
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- test/unit/resources/aws_iam_users_test.rb
+++ test/unit/resources/aws_iam_users_test.rb
@@ -1,4 +1,5 @@
 require 'helper'
+require 'ostruct'
 require 'aws_iam_users'
 
 # Maiusb = Mock AwsIamUsers::Backend
@@ -16,6 +17,24 @@ class AwsIamUsersTestConstructor < Minitest::Test
   def test_users_all_params_rejected
     assert_raises(ArgumentError) { AwsIamUsers.new(something: 'somevalue') }
   end
+end
+
+class AwsIamUsersTestFilterCriteria < Minitest::Test
+  def setup
+    # Reset to empty, that's harmless
+    AwsIamUsers::Backend.select(Maiusb::Empty)
+  end
+
+  def test_users_empty_result_when_no_users_no_criteria
+    users = AwsIamUsers.new.where { }
+    assert users.entries.empty?
+  end
+
+  def test_users_all_returned_when_some_users_no_criteria
+    AwsIamUsers::Backend.select(Maiusb::Basic)
+    users = AwsIamUsers.new.where { }
+    assert(3, users.entries.count)
+  end
 
 end
 
@@ -24,5 +43,35 @@ end
 #=============================================================================#
 module Maiusb
   class Empty < AwsIamUsers::Backend
+    def list_users
+      OpenStruct.new({
+        users: []
+      })
+    end
+  end
+
+  class Basic < AwsIamUsers::Backend
+    # arn, path, user_id omitted
+    def list_users
+      OpenStruct.new({
+        users: [
+          OpenStruct.new({
+            user_name: 'alice',
+            create_date: DateTime.parse('2017-10-10T16:19:30Z'),
+            # Password last used is absent, never logged in w/ password
+          }),
+          OpenStruct.new({
+            user_name: 'bob',
+            create_date: DateTime.parse('2017-11-06T16:19:30Z'),
+            password_last_used: DateTime.parse('2017-11-06T19:19:30Z'),
+            }),
+          OpenStruct.new({
+            user_name: 'carol',
+            create_date: DateTime.parse('2017-10-10T16:19:30Z'),
+            password_last_used: DateTime.parse('2017-10-28T19:19:30Z'),
+          }),
+        ]
+      })
+    end
   end
 end
