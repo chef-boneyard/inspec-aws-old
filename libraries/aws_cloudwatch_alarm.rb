d9211440347fc51ea32aad659f6716791a5ddec7
Clinton Wolfe <clintoncwolfe@gmail.com>
2017-11-07 15:55:33 -0500

    Constructor unit tests pass
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

---
+++ libraries/aws_cloudwatch_alarm.rb
@@ -0,0 +1,80 @@
+require 'aws_conn'
+
+class AwsCloudwatchAlarm < Inspec.resource(1)
+  name 'aws_cloudwatch_alarm'
+  desc <<-EOD
+  # Look for a specific alarm
+  aws_cloudwatch_alarm(
+    metric: 'my-metric-name',
+    metric_namespace: 'my-metric-namespace',
+  ) do
+    it { should exist }
+  end
+  EOD
+
+  attr_reader :exists, :metric_name, :metric_namespace
+  def initialize(opts)
+    # Validates and sets instance variables
+    validate_resource_params(opts)
+  end
+
+  private
+  def validate_resource_params(raw_params)
+    unless raw_params.kind_of? Hash      
+      raise ArgumentError, "Resource params should be passed using \"key: 'value'\" format."
+    end
+
+    # Currently you must specify exactly metric_name and metric_namespace
+    [:metric_name, :metric_namespace].each do |param|
+      raise ArgumentError, "Missing resource param #{param}" unless raw_params.key?(param)
+      instance_variable_set(:"@#{param}", raw_params.delete(param))
+    end
+
+    # Any leftovers are unwelcome
+    unless raw_params.empty?
+      raise ArgumentError, "Unrecognized resource param '#{raw_params.keys.first}'"
+    end
+  end
+
+  class Backend
+    #=====================================================#
+    #                    API Definition
+    #=====================================================#
+    [
+      :describe_alarms_for_metric
+    ].each do |method|
+      define_method(:method) do |*_args|
+        raise "Unimplemented abstract method #{method} - internal error"
+      end
+    end
+
+    #=====================================================#
+    #                 Concrete Implementation
+    #=====================================================#
+    # Uses the cloudwatch API to really talk to AWS
+    class AwsClientApi < Backend
+      def describe_metric_filters(criteria)
+        raise 'Unimplemented'
+      end
+    end
+
+    #=====================================================#
+    #                   Factory Interface
+    #=====================================================#
+    # TODO: move this to a mix-in
+    DEFAULT_BACKEND = AwsClientApi
+    @selected_backend = DEFAULT_BACKEND
+
+    def self.create
+      @selected_backend.new
+    end
+
+    def self.select(klass)
+      @selected_backend = klass
+    end
+
+    def self.reset
+      select(DEFAULT_BACKEND)
+    end
+  end
+end
