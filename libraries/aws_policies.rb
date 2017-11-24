67639cca32e3b2bcdec26830462dd548abbea4ab
Simon Varlow <simon.varlow@d2l.com>
2017-11-24 20:47:27 +0000

    Start of Issue 76 - AwsSupport access
    Signed-off-by: Simon Varlow <simon.varlow@d2l.com>

---
+++ libraries/aws_policies.rb
@@ -0,0 +1,30 @@
+# Authors: Simon Valow
+# Authors: Chris Redekop
+
+class AwsIamPolicy < Inspec.resource(1)
+  name 'aws_iam_policy'
+  desc 'Used to retrieve AWS IAM Policy'
+
+  example "
+    describe aws_iam_policy'AwsSupportAccess' do
+      it { should exist} 
+      its('attachment_count') { should be > 0 }
+    end
+  "
+
+  def initialize(opts, conn = AWConnection.new)
+    @policy = conn.iam_resource.policy(opts.is_a?(Hash) ? opts[:arn] : opts)
+  end
+
+  def name 
+    @policy.policy_name 
+  end
+
+  def exists?
+    !@policy.nil?
+  end
+
+  def attachment_count
+    @policy.attachment_count
+  end
+end
