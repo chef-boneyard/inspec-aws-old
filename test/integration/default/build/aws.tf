eb0ee893e82d50c490f950e134cabff3793e4639
Clinton Wolfe <clintoncwolfe@gmail.com>
2017-11-30 18:17:06 -0500

    Split IAM fixtures into their own file; add TODOs for missing tests; create naming scheme for fixtures
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- test/integration/default/build/aws.tf
+++ test/integration/default/build/aws.tf
@@ -50,28 +50,6 @@ resource "aws_instance" "no_roles_instance" {
   }
 }
 
-resource "aws_iam_user" "mfa_not_enabled_user" {
-    name = "${terraform.env}.mfa_not_enabled_user"
-}
-
-resource "aws_iam_user" "console_password_enabled_user" {
-    name = "${terraform.env}.console_password_enabled_user"
-    force_destroy = true
-}
-
-resource "aws_iam_user_login_profile" "user_login_profile" {
-  user = "${aws_iam_user.console_password_enabled_user.name}"
-  pgp_key = "${var.login_profile_pgp_key}"
-}
-
-resource "aws_iam_user" "access_key_user" {
-  name = "${terraform.env}.access_key_user"
-}
-
-resource "aws_iam_access_key" "access_key" {
-  user = "${aws_iam_user.access_key_user.name}"
-  pgp_key = "${var.login_profile_pgp_key}"
-}
 
 resource "aws_cloudwatch_log_group" "lmf_lg_1" {
   name = "${terraform.env}_lmf_lg_1"
@@ -145,22 +123,6 @@ output "lmf_lg_1_name" {
 
 output "lmf_lg_2_name" {
   value = "${aws_cloudwatch_log_group.lmf_lg_2.name}"
-}
-
-output "mfa_not_enabled_user" {
-  value = "${aws_iam_user.mfa_not_enabled_user.name}"
-}
-
-output "console_password_enabled_user" {
-  value = "${aws_iam_user.console_password_enabled_user.name}"
-}
-
-output "access_key_user" {
-  value = "${aws_iam_user.access_key_user.name}"
-}
-
-output "access_key_id" { 
-  value = "${aws_iam_access_key.access_key.id}"
 }
 
 output "example_ec2_name" {
--- test/integration/default/verify/controls/aws_iam_user.rb
+++ test/integration/default/verify/controls/aws_iam_user.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 mfa_not_enabled_user = attribute(
   'mfa_not_enabled_user',
   default: 'default.mfa_not_enabled_user',
@@ -9,10 +10,58 @@ console_password_enabled_user = attribute(
   description: 'Name of IAM user console_password_enabled_user')
 
 describe aws_iam_user(mfa_not_enabled_user) do
-  it { should_not have_mfa_enabled }
-  it { should_not have_console_password }
+=======
+
+fixtures = {}
+[
+  'iam_user_recall_hit',
+  'iam_user_recall_miss',
+  'iam_user_no_mfa_enabled',
+  'iam_user_has_console_password',
+  'iam_user_with_access_key',
+].each do |fixture_name|
+  fixtures[fixture_name] = attribute(
+    fixture_name,
+    default: "default.#{fixture_name}",
+    description: 'See ../build/iam.tf',
+  )
 end
 
+#-------------------  Recall / Miss -------------------#
+describe aws_iam_user(name: fixtures['iam_user_recall_hit']) do
+  it { should exist }
+end
+
+describe aws_iam_user(name: fixtures['iam_user_recall_miss']) do
+  it { should_not exist }
+end
+
+#------------- Property - has_mfa_enabled -------------#
+
+# TODO: fixture and test for has_mfa_enabled
+
+describe aws_iam_user(name: fixtures['iam_user_no_mfa_enabled']) do
+>>>>>>> Split IAM fixtures into their own file; add TODOs for missing tests; create naming scheme for fixtures
+  it { should_not have_mfa_enabled }
+  it { should_not have_console_password } # TODO: this is working by accident, we should have a dedicated fixture
+end
+
+<<<<<<< HEAD
 describe aws_iam_user(console_password_enabled_user) do
   it { should have_console_password }
 end
+=======
+#---------- Property - has_console_password -----------#
+
+describe aws_iam_user(name: fixtures['iam_user_has_console_password']) do
+  it { should have_console_password }
+end
+
+#------------- Property - access_keys -------------#
+
+aws_iam_user(name: fixtures['iam_user_with_access_key']).access_keys.each { |access_key|
+  describe access_key do
+   it { should be_active }
+  end
+}
+>>>>>>> Split IAM fixtures into their own file; add TODOs for missing tests; create naming scheme for fixtures
