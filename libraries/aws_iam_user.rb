bfc0ca8c499883e40f72ae9805095a4da5dfd97e
Clinton Wolfe <clintoncwolfe@gmail.com>
2017-12-04 14:02:09 -0500

    Adapt aws_iam_user to rely on AwsResourceMixin
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- libraries/aws_iam_user.rb
+++ libraries/aws_iam_user.rb
@@ -12,49 +12,10 @@ class AwsIamUser < Inspec.resource(1)
     end
   "
 
-  # This is lifted directly from resource_mixin.rb; delete after PR 121 merges
-  def initialize(resource_params)
-    validate_params(resource_params).each do |param, value|
-      instance_variable_set(:"@#{param}", value)
-    end
-    fetch_from_aws
-  end
-
-  # This is lifted directly from resource_mixin.rb; delete after PR 121 merges
-  def check_resource_param_names(raw_params: {}, allowed_params: [], allowed_scalar_name: nil, allowed_scalar_type: nil)
-    # Some resources allow passing in a single ID value.  Check and convert to hash if so.
-    if allowed_scalar_name && !raw_params.is_a?(Hash)
-      value_seen = raw_params
-      if value_seen.is_a?(allowed_scalar_type)
-        raw_params = { allowed_scalar_name => value_seen }
-      else
-        raise ArgumentError, 'If you pass a single value to the resource, it must ' \
-                             "be a #{allowed_scalar_type}, not an #{value_seen.class}."
-      end
-    end
-
-    # Remove all expected params from the raw param hash
-    validated_params = {}
-    allowed_params.each do |expected_param|
-      validated_params[expected_param] = raw_params.delete(expected_param) if raw_params.key?(expected_param)
-    end
-
-    # Any leftovers are unwelcome
-    unless raw_params.empty?
-      raise ArgumentError, "Unrecognized resource param '#{raw_params.keys.first}'. Expected parameters: #{allowed_params.join(', ')}"
-    end
-
-    validated_params
-  end
-
+  include AwsResourceMixin
   attr_reader :username, :has_mfa_enabled, :has_console_password, :access_keys
   alias has_mfa_enabled? has_mfa_enabled
   alias has_console_password? has_console_password
-
-  # This is lifted directly from resource_mixin.rb; delete after PR 121 merges
-  def exists?
-    @exists
-  end
 
   def name
     warn "[DEPRECATION] - Property ':name' is deprecated on the aws_iam_user resource.  Use ':username' instead."
@@ -84,6 +45,10 @@ class AwsIamUser < Inspec.resource(1)
     if validated_params.key?(:user)
       warn "[DEPRECATION] - Resource parameter ':user' is deprecated on the aws_iam_user resource.  Use ':aws_user_struct' instead."
       validated_params[:aws_user_struct] = validated_params.delete(:user)
+    end
+
+    if validated_params.empty?
+      raise ArgumentError, 'You must provide a username to aws_iam_user.'
     end
     validated_params
   end
@@ -118,20 +83,9 @@ class AwsIamUser < Inspec.resource(1)
     @access_keys = backend.list_access_keys(user_name: username).access_key_metadata
   end
 
-  # This class may be deleted once PR 121 is merged.
-  class BackendFactory
-    def self.create
-      @selected_backend.new
-    end
-
-    def self.select(klass)
-      @selected_backend = klass
-    end
-  end
-
   class Backend
     class AwsClientApi
-      BackendFactory.select(self) # TODO: correct to set_default_backend when 121 merges
+      BackendFactory.set_default_backend(self)
 
       def get_user(criteria)
         AWSConnection.new.iam_client.get_user(criteria)
