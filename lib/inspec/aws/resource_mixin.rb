bbc8d4bd7b4c3cef89b7d3f9d06ae1cab73b3bfc
Clinton Wolfe <clintoncwolfe@gmail.com>
2017-12-04 12:27:03 -0500

    Mixin should default to allowing empty resource params
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- lib/inspec/aws/resource_mixin.rb
+++ lib/inspec/aws/resource_mixin.rb
@@ -1,5 +1,5 @@
 module AwsResourceMixin
-  def initialize(resource_params)
+  def initialize(resource_params = {})
     validate_params(resource_params).each do |param, value|
       instance_variable_set(:"@#{param}", value)
     end
