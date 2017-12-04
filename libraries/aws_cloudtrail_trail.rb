c397877b0d941390fbb13af0fdab0ec180c61ee0
Clinton Wolfe <clintoncwolfe@gmail.com>
2017-12-04 13:16:47 -0500

    Recall unit tests working
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- libraries/aws_cloudtrail_trail.rb
+++ libraries/aws_cloudtrail_trail.rb
@@ -1,17 +1,17 @@
 class AwsCloudTrailTrail < Inspec.resource(1)
   name 'aws_cloudtrail_trail'
   desc 'Verifies individual AWS CloudTrail Trails'
-  
+
   example <<~EOD
     describe aws_cloudtrail_trail('Default') do
       its('s3_bucket_name') { should eq 'mycorp-cloudtrail' }
     end
-    
+
     describe aws_cloudtrail do
       it { should exist }
     end
   EOD
-      
+
   # This is lifted directly from resource_mixin.rb; delete after PR 121 merges
   def initialize(resource_params = {})
     validate_params(resource_params).each do |param, value|
@@ -19,7 +19,7 @@ class AwsCloudTrailTrail < Inspec.resource(1)
     end
     fetch_from_aws
   end
-    
+
   # This is lifted directly from resource_mixin.rb; delete after PR 121 merges
   def check_resource_param_names(raw_params: {}, allowed_params: [], allowed_scalar_name: nil, allowed_scalar_type: nil)
     # Some resources allow passing in a single ID value.  Check and convert to hash if so.
@@ -32,34 +32,34 @@ class AwsCloudTrailTrail < Inspec.resource(1)
         "be a #{allowed_scalar_type}, not an #{value_seen.class}."
       end
     end
-    
+
     # Remove all expected params from the raw param hash
     validated_params = {}
     allowed_params.each do |expected_param|
       validated_params[expected_param] = raw_params.delete(expected_param) if raw_params.key?(expected_param)
     end
-    
+
     # Any leftovers are unwelcome
     unless raw_params.empty?
       raise ArgumentError, "Unrecognized resource param '#{raw_params.keys.first}'. Expected parameters: #{allowed_params.join(', ')}"
     end
-    
+
     validated_params
   end
-  
-  attr_reader :trail_name 
-  
+
+  attr_reader :trail_name
+
   # This is lifted directly from resource_mixin.rb; delete after PR 121 merges
   def exists?
     @exists
   end
-  
+
   def to_s
     "CloudTrail Trail #{trail_name}"
   end
-  
+
   private
-  
+
   def validate_params(raw_params)
     validated_params = check_resource_param_names(
     raw_params: raw_params,
@@ -67,29 +67,34 @@ class AwsCloudTrailTrail < Inspec.resource(1)
     allowed_scalar_name: :trail_name,
     allowed_scalar_type: String,
     )
-    
+
     # If no name was provided, assume 'Default'.
     validated_params[:trail_name] ||= 'Default'
     validated_params
   end
-  
+
   def fetch_from_aws
     backend = BackendFactory.create
-    aws_response = backend.describe_trails(trail_name_list: [trail_name])
-    # TODO
+    trails = backend.describe_trails(trail_name_list: [trail_name]).trail_list
+    if trails.empty?
+      @exists = false
+      return
+    end
+    @exists = true
+    # TODO - properties
   end
-  
+
   # This class may be deleted once PR 121 is merged.
   class BackendFactory
     def self.create
       @selected_backend.new
     end
-    
+
     def self.select(klass)
       @selected_backend = klass
     end
   end
-  
+
   class Backend
     class AwsClientApi
       BackendFactory.select(self) # TODO: correct to set_default_backend when 121 merges
--- test/unit/resources/aws_cloudtrail_trail_test.rb
+++ test/unit/resources/aws_cloudtrail_trail_test.rb
@@ -36,6 +36,23 @@ end
 #=============================================================================#
 #                               Search / Recall
 #=============================================================================#
+class AwsCloudTrailTrailRecallTest < Minitest::Test
+
+  def test_recall_default_when_default_exists
+    AwsCloudTrailTrail::BackendFactory.select(AwsMCTTB::DefaultOnly)
+    assert(AwsCloudTrailTrail.new.exists?)
+  end
+
+  def test_recall_default_when_default_absent
+    AwsCloudTrailTrail::BackendFactory.select(AwsMCTTB::TwoTrails)
+    refute(AwsCloudTrailTrail.new.exists?)
+  end
+
+  def test_recall_custom_when_multiple_exist
+    AwsCloudTrailTrail::BackendFactory.select(AwsMCTTB::TwoTrails)
+    assert(AwsCloudTrailTrail.new('applications').exists?)
+  end
+end
 
 #=============================================================================#
 #                                Properties
@@ -46,11 +63,49 @@ end
 #=============================================================================#
 
 module AwsMCTTB
+  def filter_trails(data, query)
+    matched_trails = data.trail_list.select do |trail|
+      query[:trail_name_list].any? {|name| trail.name == name }
+    end
+    OpenStruct.new(trail_list: matched_trails)
+  end
+  module_function :filter_trails
+
   class NoTrails < AwsCloudTrailTrail::Backend
     def describe_trails(query)
-      OpenStruct.new({
-        trails: []
+      data = OpenStruct.new({
+        trail_list: []
       })
+      AwsMCTTB::filter_trails(data, query)
+    end
+  end
+
+  class DefaultOnly < AwsCloudTrailTrail::Backend
+    def describe_trails(query)
+      data = OpenStruct.new({
+        trail_list: [
+          OpenStruct.new({
+            name: 'Default',
+          }),
+        ]
+      })
+      AwsMCTTB::filter_trails(data, query)
+    end
+  end
+
+  class TwoTrails < AwsCloudTrailTrail::Backend
+    def describe_trails(query)
+      data = OpenStruct.new({
+        trail_list: [
+          OpenStruct.new({
+            name: 'security',
+          }),
+          OpenStruct.new({
+            name: 'applications',
+          }),
+        ]
+      })
+      AwsMCTTB::filter_trails(data, query)
     end
   end
 end
