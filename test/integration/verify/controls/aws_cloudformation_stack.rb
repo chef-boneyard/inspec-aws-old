fe22983a50bb1d72227ccb294cdb6ac058796875
Aaron Lippold <lippold@gmail.com>
2017-12-04 17:59:11 -0500

    Update aws_cloudformation_stack.rb
    small spacing errors on examples
    
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

--- test/integration/verify/controls/aws_cloudformation_stack.rb
+++ test/integration/verify/controls/aws_cloudformation_stack.rb
@@ -1,7 +1,8 @@
+
 describe aws_cloudformation_stack do
   its('entries') { should eq [] }
 end
 
-describe aws_cloudformation_stack.where { stack_status != 'CREATE_COMPLETE'} do
-  its('entries') { should eq []}
+describe aws_cloudformation_stack.where { stack_status != 'CREATE_COMPLETE' } do
+  its('entries') { should eq [] }
 end
