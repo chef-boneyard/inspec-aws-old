6258648c199a8a6e6e51c58af3a43012cb28ea1d
Simon Varlow <simon.varlow@d2l.com>
2017-12-07 16:57:21 +0000

    adding in an addition integration test
    Signed-off-by: Simon Varlow <simon.varlow@d2l.com>

--- test/integration/verify/controls/aws_iam_policy.rb
+++ test/integration/verify/controls/aws_iam_policy.rb
@@ -1,4 +1,5 @@
 describe aws_iam_policy(arn: 'arn:aws:iam::aws:policy/AWSSupportAccess') do 
 	it { should exist }
 	its('attachment_count') { should be > 0 }
+	its('name') {should eq 'AWSSupportAccess'}
 end
