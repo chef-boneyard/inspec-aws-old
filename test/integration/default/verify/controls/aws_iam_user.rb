e7407db2abbcdcf0260aca44e3f190f3ecc3172b
Clinton Wolfe <clintoncwolfe@gmail.com>
2017-12-15 00:53:24 -0500

    Remove merge conflict markers
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- test/integration/default/verify/controls/aws_iam_user.rb
+++ test/integration/default/verify/controls/aws_iam_user.rb
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-mfa_not_enabled_user = attribute(
-  'mfa_not_enabled_user',
-  default: 'default.mfa_not_enabled_user',
-  description: 'Name of IAM user mfa_not_enabled_user')
-
-console_password_enabled_user = attribute(
-  'console_password_enabled_user',
-  default: 'default.console_password_enabled_user',
-  description: 'Name of IAM user console_password_enabled_user')
-
-describe aws_iam_user(mfa_not_enabled_user) do
-=======
 
 fixtures = {}
 [
@@ -41,16 +28,10 @@ end
 # TODO: fixture and test for has_mfa_enabled
 
 describe aws_iam_user(name: fixtures['iam_user_no_mfa_enabled']) do
->>>>>>> Split IAM fixtures into their own file; add TODOs for missing tests; create naming scheme for fixtures
   it { should_not have_mfa_enabled }
   it { should_not have_console_password } # TODO: this is working by accident, we should have a dedicated fixture
 end
 
-<<<<<<< HEAD
-describe aws_iam_user(console_password_enabled_user) do
-  it { should have_console_password }
-end
-=======
 #---------- Property - has_console_password -----------#
 
 describe aws_iam_user(name: fixtures['iam_user_has_console_password']) do
@@ -64,4 +45,3 @@ aws_iam_user(name: fixtures['iam_user_with_access_key']).access_keys.each { |acc
    it { should be_active }
   end
 }
->>>>>>> Split IAM fixtures into their own file; add TODOs for missing tests; create naming scheme for fixtures
