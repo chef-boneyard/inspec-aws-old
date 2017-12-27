8be2f8328719c01d1e51989d739e925d2bc768d9
Matthew Dromazos <dromazmj@dukes.jmu.edu>
2017-12-27 12:54:55 -0500

    Undoes last commit
    Signed-off-by: Matthew Dromazos <dromazmj@dukes.jmu.edu>

--- test/integration/default/verify/controls/aws_s3_buckets.rb
+++
@@ -1,8 +0,0 @@
-
-
-control 'aws_s3_buckets public buckets' do
-  #------------------- Exists / Permissions Owner / public files  -------------------#
-  describe aws_s3_bucket do
-    it { should exist }
-  end
-end
