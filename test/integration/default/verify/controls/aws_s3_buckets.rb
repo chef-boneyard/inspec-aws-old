d39b9ae792f220001a2647418ae5058f77f0b4bb
Matthew Dromazos <dromazmj@dukes.jmu.edu>
2017-12-27 12:52:13 -0500

    Cleans up test
    Signed-off-by: Matthew Dromazos <dromazmj@dukes.jmu.edu>

--- test/integration/default/verify/controls/aws_s3_buckets.rb
+++ test/integration/default/verify/controls/aws_s3_buckets.rb
@@ -2,9 +2,7 @@
 
 control 'aws_s3_buckets public buckets' do
   #------------------- Exists / Permissions Owner / public files  -------------------#
-  describe aws_s3_bucket(name: fixtures['s3_bucket_name']) do
+  describe aws_s3_bucket do
     it { should exist }
-    it { should have_public_files }
-    its('permissions.owner') { should be_in ['FULL_CONTROL'] }
   end
 end
