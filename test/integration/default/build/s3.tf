db42b4e79b05ac74954752da42a8336571438a32
Clinton Wolfe <clintoncwolfe@gmail.com>
2018-01-11 23:56:49 -0800

    Adjust integration test fixtures for completeness
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- test/integration/default/build/s3.tf
+++ test/integration/default/build/s3.tf
@@ -1,49 +1,73 @@
-resource "aws_s3_bucket" "aws_demo_bucket_public" {
-  bucket        = "aws-demo-s3-bucket-public-${terraform.env}.chef.io"
+
+#=================================================================#
+#                          S3 Bucket
+#=================================================================#
+
+resource "aws_s3_bucket" "public" {
+  bucket        = "inspec-testing-public-${terraform.env}.chef.io"
   acl           = "public-read"
-  force_destroy = true
-  region        = "${var.region}"
 }
 
-output "s3_bucket_name_public" {
-  value = "aws-demo-s3-bucket-public-${terraform.env}.chef.io"
+output "s3_bucket_public_name" {
+  value = "${aws_s3_bucket.public.id}"
 }
 
-# add s3 bucket elements - pub
+output "s3_bucket_public_region" {
+  value = "${aws_s3_bucket.public.region}"
+}
+
+resource "aws_s3_bucket" "private" {
+  bucket        = "inspec-testing-private-${terraform.env}.chef.io"
+  acl           = "private"
+}
+
+output "s3_bucket_private_name" {
+  value = "${aws_s3_bucket.private.id}"
+}
+
+resource "aws_s3_bucket" "auth" {
+  bucket        = "inspec-testing-auth-${terraform.env}.chef.io"
+  acl           = "authenticated-read"
+}
+
+output "s3_bucket_auth_name" {
+  value = "${aws_s3_bucket.auth.id}"
+}
+
+#=================================================================#
+#                       S3 Bucket Objects
+#=================================================================#
+
+# valid ACLs are "authenticated-read", "aws-exec-read", "bucket-owner-full-control", 
+# "bucket-owner-read", "private", "public-read", or "public-read-write"
 
 resource "aws_s3_bucket_object" "public-read" {
-  bucket = "${aws_s3_bucket.aws_demo_bucket_public.id}"
+  bucket = "${aws_s3_bucket.public.id}"
   acl    = "public-read"
-  key    = "public-pic-read.jpg"
-  source = "./data/public-pic.jpg"
-  etag   = "${md5(file("./data/public-pic.jpg"))}"
-
-  depends_on = ["aws_s3_bucket.aws_demo_bucket_public"]
+  key    = "public-pic.png"
+  source = "./inspec-logo.png"  
 }
 
-# add s3 bucket elements - pub Authenticated Users only
-
+# add s3 bucket objects - pub Authenticated Users only
 resource "aws_s3_bucket_object" "authenticated-read" {
-  bucket = "${aws_s3_bucket.aws_demo_bucket_public.id}"
+  bucket = "${aws_s3_bucket.public.id}"
   acl    = "authenticated-read"
-  key    = "public-pic-authenticated.jpg"
-  source = "./data/public-pic.jpg"
-  etag   = "${md5(file("./data/public-pic.jpg"))}"
-
-  depends_on = ["aws_s3_bucket.aws_demo_bucket_public"]
+  key    = "auth-pic.png"
+  source = "./inspec-logo.png"  
 }
-# add s3 bucket elements - pri
 
+# add s3 bucket objects - private in public
 resource "aws_s3_bucket_object" "private" {
-  bucket = "${aws_s3_bucket.aws_demo_bucket_public.id}"
+  bucket = "${aws_s3_bucket.public.id}"
   acl = "private"
-  key    = "private-pic.jpg"
-  source = "./data/private-pic.jpg"
-  etag   = "${md5(file("./data/private-pic.jpg"))}"
-
-  depends_on = ["aws_s3_bucket.aws_demo_bucket_public"]
+  key    = "private-pic.png"
+  source = "./inspec-logo.png"  
 }
 
-# valid ACLs are Error: aws_s3_bucket_object.public: "acl" contains an invalid canned ACL type "public". V
-#alid types are either "authenticated-read", "aws-exec-read", "bucket-owner-full-control", "bucket-owner-read",
-# "private", "public-read", or "public-read-write"
+# add s3 bucket objects - private in private
+resource "aws_s3_bucket_object" "private" {
+  bucket = "${aws_s3_bucket.private.id}"
+  acl = "private"
+  key    = "private-pic.png"
+  source = "./inspec-logo.png"
+}
--- test/integration/default/verify/controls/aws_s3_bucket.rb
+++ test/integration/default/verify/controls/aws_s3_bucket.rb
@@ -1,6 +1,9 @@
 fixtures = {}
 [
-  's3_bucket_name',
+  's3_bucket_public_name',
+  's3_bucket_private_name',
+  's3_bucket_auth_name',
+  's3_bucket_public_region',
 ].each do |fixture_name|
   fixtures[fixture_name] = attribute(
     fixture_name,
@@ -11,30 +14,58 @@ end
 
 control 'aws_s3_bucket recall tests' do
   #------------------- Exists -------------------#
-  describe aws_s3_bucket(bucket_name: fixtures['s3_bucket_name_public']) do
+  describe aws_s3_bucket(bucket_name: fixtures['s3_bucket_public_name']) do
     it { should exist }
   end
+
   #------------------- Does Not Exist -------------------#
-  describe aws_s3_bucket(bucket_name: 'NonExistentBucket') do
+  describe aws_s3_bucket(bucket_name: 'inspec-testing-NonExistentBucket.chef.io') do
     it { should_not exist }
   end
 end
 
 control 'aws_s3_bucket properties tests' do
-  #------------------- Exists / Permissions Owner / public files  -------------------#
-  describe aws_s3_bucket(bucket_name: fixtures['s3_bucket_name_public']) do
+  #--------------------------- Region --------------------------#
+  describe aws_s3_bucket(bucket_name: fixtures['s3_bucket_public_name']) do
+    its('region') { should eq fixtures['s3_bucket_public_region'] }
+  end
+
+  #-------------------  public_objects  -------------------#
+  describe aws_s3_bucket(bucket_name: fixtures['s3_bucket_public_name']) do
+    its('public_objects') { should include "public-pic.png" }
+  end
+  describe aws_s3_bucket(bucket_name: fixtures['s3_bucket_private_name']) do
+    its('public_objects') { should be_empty }
+  end
+
+  #------------------- Permissions Owner -------------------#
+  # TODO
+  describe aws_s3_bucket(bucket_name: fixtures['s3_bucket_public_name']) do
     its('permissions.owner') { should be_in ['FULL_CONTROL'] }
     its('permissions.authUsers') { should be_in [] }
     its('permissions.logGroup') { should be_in [] }
     its('permissions.everyone') { should be_in ['READ'] }
-    its('region') { should eq 'us-east-1' }
-    its('public_objects') { should eq ["public-pic-read.jpg"] }
   end
 end
 
 control 'aws_s3_bucket matchers test' do
-  describe aws_s3_bucket(bucket_name: fixtures['s3_bucket_name_public']) do
-    it { should have_public_files }
+  
+  #------------------------  be_public --------------------------#  
+  describe aws_s3_bucket(bucket_name: fixtures['s3_bucket_public_name']) do
     it { should be_public }
+  end
+  describe aws_s3_bucket(bucket_name: fixtures['s3_bucket_auth_name']) do
+    it { should be_public }
+  end
+  describe aws_s3_bucket(bucket_name: fixtures['s3_bucket_private_name']) do
+    it { should_not be_public }
+  end
+
+  #------------------------ has_public_objects --------------------------#  
+  describe aws_s3_bucket(bucket_name: fixtures['s3_bucket_public_name']) do
+    it { should have_public_objects }
+  end
+  describe aws_s3_bucket(bucket_name: fixtures['s3_bucket_private_name']) do
+    it { should_not have_public_objects }
   end
 end
