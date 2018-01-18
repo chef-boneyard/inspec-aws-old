3b84aa95f19120cd9dd23b5a95ff2972f22a8d87
Rony Xavier <rx294@nyu.edu>
2018-01-18 14:21:24 -0500

    Updates
    Signed-off-by: Rony Xavier <rx294@nyu.edu>

--- test/unit/resources/aws_kms_key_test.rb
+++ test/unit/resources/aws_kms_key_test.rb
@@ -19,11 +19,11 @@ class AwsKmsKeyConstructorTest < Minitest::Test
   end
   
   def test_accepts_key_arn_as_scalar
-    AwsKmsKey.new('test-key-1-arn')
+    AwsKmsKey.new('arn:aws:kms:us-east-1::key/7a6950aa-c8e6-4e51-8afc-111111111111')
   end
   
   def test_accepts_key_arn_as_hash
-    AwsKmsKey.new(key_arn: 'test-key-1-arn')
+    AwsKmsKey.new(key_arn: 'arn:aws:kms:us-east-1::key/7a6950aa-c8e6-4e51-8afc-111111111111')
   end
   
   def test_rejects_unrecognized_params
@@ -41,11 +41,11 @@ class AwsKmsKeyRecallTest < Minitest::Test
   end
   
   def test_search_hit_via_scalar_works
-    assert AwsKmsKey.new('test-key-1-arn').exists?
+    assert AwsKmsKey.new('arn:aws:kms:us-east-1::key/7a6950aa-c8e6-4e51-8afc-111111111111').exists?
   end
   
   def test_search_hit_via_hash_works
-    assert AwsKmsKey.new(key_arn: 'test-key-1-arn').exists?
+    assert AwsKmsKey.new(key_arn: 'arn:aws:kms:us-east-1::key/7a6950aa-c8e6-4e51-8afc-111111111111').exists?
   end
   
   def test_search_miss_is_not_an_exception
@@ -63,62 +63,62 @@ class AwsKmsKeyPropertiesTest < Minitest::Test
   end
 
   def test_property_key_id
-    assert_equal('test-key-1', AwsKmsKey.new('test-key-1-arn').key_id)
+    assert_equal('7a6950aa-c8e6-4e51-8afc-111111111111', AwsKmsKey.new('arn:aws:kms:us-east-1::key/7a6950aa-c8e6-4e51-8afc-111111111111').key_id)
     assert_nil(AwsKmsKey.new(key_arn: 'non-existant').key_id)
   end
   
   def test_property_arn
-    assert_equal('test-key-1-arn', AwsKmsKey.new('test-key-1-arn').arn)
+    assert_equal('arn:aws:kms:us-east-1::key/7a6950aa-c8e6-4e51-8afc-111111111111', AwsKmsKey.new('arn:aws:kms:us-east-1::key/7a6950aa-c8e6-4e51-8afc-111111111111').arn)
     assert_nil(AwsKmsKey.new(key_arn: 'non-existant').arn)
   end
   
   def test_property_creation_date
-    assert_equal(TIME_NOW - 10*24*3600, AwsKmsKey.new('test-key-1-arn').creation_date)
+    assert_equal(TIME_NOW - 10*24*3600, AwsKmsKey.new('arn:aws:kms:us-east-1::key/7a6950aa-c8e6-4e51-8afc-111111111111').creation_date)
     assert_nil(AwsKmsKey.new(key_arn: 'non-existant').creation_date)
   end
   
   def test_property_key_usage
-    assert_equal('ENCRYPT_DECRYPT', AwsKmsKey.new('test-key-1-arn').key_usage)
+    assert_equal('ENCRYPT_DECRYPT', AwsKmsKey.new('arn:aws:kms:us-east-1::key/7a6950aa-c8e6-4e51-8afc-111111111111').key_usage)
     assert_nil(AwsKmsKey.new(key_arn: 'non-existant').key_usage)
   end
   
   def test_property_key_state
-    assert_equal('Enabled', AwsKmsKey.new('test-key-1-arn').key_state)
+    assert_equal('Enabled', AwsKmsKey.new('arn:aws:kms:us-east-1::key/7a6950aa-c8e6-4e51-8afc-111111111111').key_state)
     assert_nil(AwsKmsKey.new(key_arn: 'non-existant').key_state)
   end
   
   def test_property_description
-    assert_equal('test-key-1-desc', AwsKmsKey.new('test-key-1-arn').description)
+    assert_equal('test-key-1-desc', AwsKmsKey.new('arn:aws:kms:us-east-1::key/7a6950aa-c8e6-4e51-8afc-111111111111').description)
     assert_nil(AwsKmsKey.new(key_arn: 'non-existant').description)
   end
   
   def test_property_deletion_date
-    assert_equal(TIME_NOW + 10*24*3600, AwsKmsKey.new('test-key-1-arn').deletion_date)
+    assert_equal(TIME_NOW + 10*24*3600, AwsKmsKey.new('arn:aws:kms:us-east-1::key/7a6950aa-c8e6-4e51-8afc-111111111111').deletion_date)
     assert_nil(AwsKmsKey.new(key_arn: 'non-existant').deletion_date)
   end
   
   def test_property_valid_to
-    assert_equal(nil, AwsKmsKey.new('test-key-1-arn').valid_to)
+    assert_equal(nil, AwsKmsKey.new('arn:aws:kms:us-east-1::key/7a6950aa-c8e6-4e51-8afc-111111111111').valid_to)
     assert_nil(AwsKmsKey.new(key_arn: 'non-existant').valid_to)
   end
   
   def test_property_origin
-    assert_equal('AWS_KMS', AwsKmsKey.new('test-key-1-arn').origin)
+    assert_equal('AWS_KMS', AwsKmsKey.new('arn:aws:kms:us-east-1::key/7a6950aa-c8e6-4e51-8afc-111111111111').origin)
     assert_nil(AwsKmsKey.new(key_arn: 'non-existant').origin)
   end
   
   def test_property_expiration_model
-    assert_equal('KEY_MATERIAL_EXPIRES', AwsKmsKey.new('test-key-1-arn').expiration_model)
+    assert_equal('KEY_MATERIAL_EXPIRES', AwsKmsKey.new('arn:aws:kms:us-east-1::key/7a6950aa-c8e6-4e51-8afc-111111111111').expiration_model)
     assert_nil(AwsKmsKey.new(key_arn: 'non-existant').expiration_model)
   end
   
   def test_property_key_manager
-    assert_equal('AWS', AwsKmsKey.new('test-key-1-arn').key_manager)
+    assert_equal('AWS', AwsKmsKey.new('arn:aws:kms:us-east-1::key/7a6950aa-c8e6-4e51-8afc-111111111111').key_manager)
     assert_nil(AwsKmsKey.new(key_arn: 'non-existant').key_manager)
   end
   
   def test_property_created_days_ago
-    assert_equal(10, AwsKmsKey.new('test-key-1-arn').created_days_ago)
+    assert_equal(10, AwsKmsKey.new('arn:aws:kms:us-east-1::key/7a6950aa-c8e6-4e51-8afc-111111111111').created_days_ago)
     assert_nil(AwsKmsKey.new(key_arn: 'non-existant').created_days_ago)
   end
 end
@@ -133,19 +133,19 @@ class AwsKmsKeyMatchersTest < Minitest::Test
   end
   
   def test_matcher_enabled_positive
-    assert AwsKmsKey.new('test-key-1-arn').enabled?
+    assert AwsKmsKey.new('arn:aws:kms:us-east-1::key/7a6950aa-c8e6-4e51-8afc-111111111111').enabled?
   end
 
   def test_matcher_enabled_negative
-    refute AwsKmsKey.new('test-key-2-arn').enabled?
+    refute AwsKmsKey.new('arn:aws:kms:us-east-1::key/7a6950aa-c8e6-4e51-8afc-222222222222').enabled?
   end
   
   def test_matcher_rotation_enabled_positive
-    assert AwsKmsKey.new('test-key-1-arn').rotation_enabled?
+    assert AwsKmsKey.new('arn:aws:kms:us-east-1::key/7a6950aa-c8e6-4e51-8afc-111111111111').rotation_enabled?
   end
 
   def test_matcher_rotation_enabled_negative
-    refute AwsKmsKey.new('test-key-2-arn').rotation_enabled?
+    refute AwsKmsKey.new('arn:aws:kms:us-east-1::key/7a6950aa-c8e6-4e51-8afc-222222222222').rotation_enabled?
   end
 end
 
@@ -164,8 +164,8 @@ module MAKKSB
     def describe_key(query)
       fixtures = [
        OpenStruct.new({
-           key_id: "test-key-1",
-           arn: "test-key-1-arn",
+           key_id: "7a6950aa-c8e6-4e51-8afc-111111111111",
+           arn: "arn:aws:kms:us-east-1::key/7a6950aa-c8e6-4e51-8afc-111111111111",
            creation_date: TIME_NOW - 10*24*3600,
            enabled: true,
            description: "test-key-1-desc",
@@ -178,8 +178,8 @@ module MAKKSB
            key_manager: "AWS"
         }),
         OpenStruct.new({
-           key_id: "test-key-2",
-           arn: "test-key-2-arn",
+           key_id: "7a6950aa-c8e6-4e51-8afc-222222222222",
+           arn: "arn:aws:kms:us-east-1::key/7a6950aa-c8e6-4e51-8afc-222222222222",
            creation_date: TIME_NOW,
            enabled: false,
            description: "test-key-2-desc",
@@ -202,11 +202,11 @@ module MAKKSB
     def get_key_rotation_status(query)
       fixtures = [
        OpenStruct.new({
-           arn: "test-key-1-arn",
+           arn: "arn:aws:kms:us-east-1::key/7a6950aa-c8e6-4e51-8afc-111111111111",
            key_rotation_enabled: true
         }),
         OpenStruct.new({
-           arn: "test-key-2-arn",
+           arn: "arn:aws:kms:us-east-1::key/7a6950aa-c8e6-4e51-8afc-222222222222",
            key_rotation_enabled: false
         }),
       ]
